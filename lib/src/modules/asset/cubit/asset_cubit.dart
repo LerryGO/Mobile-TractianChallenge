ff1c5480e7cfcf93a66c3e62a4138310dd86614f
Lerry Augusto <lerry.pirani@gmail.com>
2024-07-06 17:14:58 -0300

    asset tree created

--- lib/src/modules/asset/cubit/asset_cubit.dart
+++ lib/src/modules/asset/cubit/asset_cubit.dart
@@ -3,6 +3,9 @@ import 'package:equatable/equatable.dart';
 import 'package:tractian_challenge/src/repositories/asset/asset_repository.dart';
 import 'package:tractian_challenge/src/repositories/location/location_repository.dart';
 
+import '../../../models/asset_model.dart';
+import '../../../models/location_model.dart';
+
 part 'asset_state.dart';
 
 class AssetCubit extends Cubit<AssetState> {
@@ -17,15 +20,57 @@ class AssetCubit extends Cubit<AssetState> {
         super(AssetInitial());
 
   Future<void> init(String companyId) async {
-    await _fetchLocations(companyId);
-    await _fetchAssets(companyId);
+    final locations = await _fetchLocations(companyId);
+    final assets = await _fetchAssets(companyId);
+    _fillData(locations, assets);
   }
 
-  Future<void> _fetchLocations(String companyId) async {
+  Future<List<LocationModel>> _fetchLocations(String companyId) async {
     final result = await _locationRepository.fetchLocations(companyId);
+    return result ?? [];
   }
 
-  Future<void> _fetchAssets(String companyId) async {
+  Future<List<AssetModel>> _fetchAssets(String companyId) async {
     final result = await _assetRepository.fetchAssets(companyId);
+    return result ?? [];
+  }
+
+  void _fillData(List<LocationModel> locations, List<AssetModel> assets) async {
+    final locationList = [...locations];
+    final assetsList = [...assets];
+    final assetToRemove = [];
+    final finalList = <dynamic>[];
+
+    for (AssetModel asset in assets) {
+      if (asset.parentId != null) {
+        final index = assetsList.indexWhere(
+          (element) => element.id == asset.parentId,
+        );
+        if (index != -1) {
+          assetsList[index].assetList.add(asset);
+          assetToRemove.add(asset);
+        }
+      }
+    }
+
+    for (AssetModel asset in assetsList) {
+      if (asset.locationId != null) {
+        final index = locationList.indexWhere(
+          (element) => element.id == asset.locationId,
+        );
+        if (index != -1) {
+          locationList[index].assetLists.add(asset);
+          assetToRemove.add(asset);
+        }
+      }
+    }
+
+    for (var asset in assetToRemove) {
+      assetsList.remove(asset);
+    }
+
+    finalList.addAll([...assetsList, ...locationList]);
+
+    emit(AssetLoaded(dataList: finalList));
   }
 }
--- lib/src/modules/asset/cubit/asset_state.dart
+++ lib/src/modules/asset/cubit/asset_state.dart
@@ -1,10 +1,27 @@
 part of 'asset_cubit.dart';
 
 sealed class AssetState extends Equatable {
-  const AssetState();
+  const AssetState() : datas = const [];
+  final List<dynamic> datas;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [datas];
 }
 
 final class AssetInitial extends AssetState {}
+
+final class AssetLoading extends AssetState {}
+
+final class AssetLoaded extends AssetState {
+  final List<dynamic> dataList;
+
+  AssetLoaded({required this.dataList}) {
+    super.datas.clear();
+    super.datas.addAll(dataList);
+  }
+}
+
+final class AssetError extends AssetState {
+  final String? message;
+  const AssetError({this.message});
+}
